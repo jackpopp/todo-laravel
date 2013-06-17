87af37f1fd7e66559a4e50b87810a27dc2502714
Jack Hannigan Popp <jack.popp@gmail.com>
2013-06-17 07:07:41 +0100

    Added show one list function.

--- app/controllers/TodoListController.php
+++ app/controllers/TodoListController.php
@@ -3,18 +3,36 @@
 class TodoListController extends BaseController {
 	
 	/**
-	* Show the lists belonging to the logged in user
+	* Show the lists belonging to the logged in user.
+	* Eventually add shared lists.
+	* @return JSON array.
 	**/
 
 	public function index()
 	{
-		$user = Auth::user();
-		print_r($user->todoLists()->get());
+		$list = Auth::user()->todoLists()->get();
+		Response::json(array('success' => true, 'list' => $list ), 200);
 	}
 
 	public function show($id)
 	{
-		echo $id;
+		$list = TodoList::find($id);
+		if ($list)
+		{
+			if(Auth::user()->id == $list->user_id)
+			{
+				return Response::json(array('success'=>true,'list'=>$list), 200);
+			}
+			else
+			{
+				return Response::json(array('success' => false, 'message' => 'Not authorised to view list.'), 401);
+			}
+		}	
+		else
+		{
+			return Response::json(array('success' => false, 'message' => 'Not found.'), 404);
+		}
+			
 	}
 
 	public function edit()
