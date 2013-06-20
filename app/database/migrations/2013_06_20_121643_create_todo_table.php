cd5aca1e82fe6f86e6aca6f1f83bcfd18e6548c5
Jack Hannigan Popp <jack@ethosdigital.co.uk>
2013-06-20 13:19:25 +0100

    Added migration to create todo table

---
+++ app/database/migrations/2013_06_20_121643_create_todo_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('todos', function($table)
+		{
+			$table->increments('id');
+			$table->integer('user_id');
+			$table->integer('list_id');
+		    $table->string('title');
+		    $table->string('summary');
+		    $table->integer('completed');
+		    $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('todos');
+	}
+
+}
